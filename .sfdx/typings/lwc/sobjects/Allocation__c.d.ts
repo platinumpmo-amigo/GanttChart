4cbf92c9c160e95a924c385b77c1195899bed5af
Sourav Saha <sourav.saha@platinumpmo.com>
2021-02-02 18:10:34 +0530

    Refrsh

---
+++ .sfdx/typings/lwc/sobjects/Allocation__c.d.ts
@@ -0,0 +1,80 @@
+declare module "@salesforce/schema/Allocation__c.Id" {
+  const Id:any;
+  export default Id;
+}
+declare module "@salesforce/schema/Allocation__c.IsDeleted" {
+  const IsDeleted:boolean;
+  export default IsDeleted;
+}
+declare module "@salesforce/schema/Allocation__c.Name" {
+  const Name:string;
+  export default Name;
+}
+declare module "@salesforce/schema/Allocation__c.CreatedDate" {
+  const CreatedDate:any;
+  export default CreatedDate;
+}
+declare module "@salesforce/schema/Allocation__c.CreatedBy" {
+  const CreatedBy:any;
+  export default CreatedBy;
+}
+declare module "@salesforce/schema/Allocation__c.CreatedById" {
+  const CreatedById:any;
+  export default CreatedById;
+}
+declare module "@salesforce/schema/Allocation__c.LastModifiedDate" {
+  const LastModifiedDate:any;
+  export default LastModifiedDate;
+}
+declare module "@salesforce/schema/Allocation__c.LastModifiedBy" {
+  const LastModifiedBy:any;
+  export default LastModifiedBy;
+}
+declare module "@salesforce/schema/Allocation__c.LastModifiedById" {
+  const LastModifiedById:any;
+  export default LastModifiedById;
+}
+declare module "@salesforce/schema/Allocation__c.SystemModstamp" {
+  const SystemModstamp:any;
+  export default SystemModstamp;
+}
+declare module "@salesforce/schema/Allocation__c.LastViewedDate" {
+  const LastViewedDate:any;
+  export default LastViewedDate;
+}
+declare module "@salesforce/schema/Allocation__c.LastReferencedDate" {
+  const LastReferencedDate:any;
+  export default LastReferencedDate;
+}
+declare module "@salesforce/schema/Allocation__c.Resource__r" {
+  const Resource__r:any;
+  export default Resource__r;
+}
+declare module "@salesforce/schema/Allocation__c.Resource__c" {
+  const Resource__c:any;
+  export default Resource__c;
+}
+declare module "@salesforce/schema/Allocation__c.Effort__c" {
+  const Effort__c:string;
+  export default Effort__c;
+}
+declare module "@salesforce/schema/Allocation__c.End_Date__c" {
+  const End_Date__c:any;
+  export default End_Date__c;
+}
+declare module "@salesforce/schema/Allocation__c.Project__r" {
+  const Project__r:any;
+  export default Project__r;
+}
+declare module "@salesforce/schema/Allocation__c.Project__c" {
+  const Project__c:any;
+  export default Project__c;
+}
+declare module "@salesforce/schema/Allocation__c.Start_Date__c" {
+  const Start_Date__c:any;
+  export default Start_Date__c;
+}
+declare module "@salesforce/schema/Allocation__c.Status__c" {
+  const Status__c:string;
+  export default Status__c;
+}
