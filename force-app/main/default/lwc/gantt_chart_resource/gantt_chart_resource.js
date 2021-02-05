bc3c1e7f288beb697c67402aa37765109ee815d8
Sourav Saha <sourav.saha@platinumpmo.com>
2021-02-05 15:10:59 +0530

    SFDX Force Defination Refresh

--- force-app/main/default/lwc/gantt_chart_resource/gantt_chart_resource.js
+++ force-app/main/default/lwc/gantt_chart_resource/gantt_chart_resource.js
@@ -236,11 +236,13 @@ export default class GanttChartResource extends LightningElement {
         allocations: []
       };
 
-      self.resource.allocationsByProject[projectId].forEach(allocation => {
+      self.resource.allocationsByProject[projectId].forEach(allocation2 => {
+
+        let allocation = JSON.parse(JSON.stringify(allocation2))
         allocation.class = self.calcClass(allocation);
         allocation.style = self.calcStyle(allocation);
         allocation.labelStyle = self.calcLabelStyle(allocation);
-
+  
         project.allocations.push(allocation);
       });
 
