70f426d31bb398a60257f1aaf985abb241b0de43
Sourav Saha <sourav.saha@platinumpmo.com>
2021-03-20 02:41:29 +0530

    Project Edit DIsable Complete

--- force-app/main/default/lwc/gantt_chart_resource/gantt_chart_resource.js
+++ force-app/main/default/lwc/gantt_chart_resource/gantt_chart_resource.js
@@ -93,30 +93,7 @@ export default class GanttChartResource extends LightningElement {
 
   @track projects = [];
 
-  effortOptions = [
-    {
-      label: "Low",
-      value: "Low"
-    },
-    {
-      label: "Medium",
-      value: "Medium"
-    },
-    {
-      label: "High",
-      value: "High"
-    }
-  ];
-  statusOptions = [
-    {
-      label: "Active",
-      value: "Active"
-    },
-    {
-      label: "Hold",
-      value: "Hold"
-    }
-  ];
+  
 
   connectedCallback() {
     this.refreshDates(this.startDate, this.endDate, this.dateIncrement);
@@ -185,16 +162,13 @@ export default class GanttChartResource extends LightningElement {
         Lime: "#7CCC47",
         Gold: "#FCAF32"
       };
-      styles.push("background-color: " + colorMap[backgroundColor]);
+      styles.push("background-color: #1589EE");
     }
 
-    if (!isNaN(this.dragInfo.startIndex)) {
-      styles.push("pointer-events: none");
-      styles.push("transition: left ease 250ms, right ease 250ms");
-    } else {
+    
       styles.push("pointer-events: auto");
       styles.push("transition: none");
-    }
+    
 
     return styles.join("; ");
   }
@@ -217,11 +191,9 @@ export default class GanttChartResource extends LightningElement {
       "right: calc(" + right * 100 + "% + 30px)"
     ];
 
-    if (!isNaN(this.dragInfo.startIndex)) {
-      styles.push("transition: left ease 250ms, right ease 250ms");
-    } else {
+    
       styles.push("transition: none");
-    }
+    
 
     return styles.join("; ");
   }
@@ -249,7 +221,9 @@ export default class GanttChartResource extends LightningElement {
         allocation.class = self.calcClass(allocation);
         allocation.style = self.calcStyle(allocation);
         allocation.labelStyle = self.calcLabelStyle(allocation);
-  
+        console.log("Allocation Class : "+allocation.class);
+        console.log("Allocation Style : "+allocation.style);
+        console.log("Allocation Label Style : "+allocation.labelStyle);
         project.allocations.push(allocation);
       });
 
@@ -257,364 +231,20 @@ export default class GanttChartResource extends LightningElement {
     });
   }
 
-  handleTimeslotClick(event) {
-    const start = new Date(parseInt(event.currentTarget.dataset.start, 10));
-    const end = new Date(parseInt(event.currentTarget.dataset.end, 10));
-    const startUTC = start.getTime() + start.getTimezoneOffset() * 60 * 1000;
-    const endUTC = end.getTime() + end.getTimezoneOffset() * 60 * 1000;
 
-    if (this.projectId) {
-      this._saveAllocation({
-        startDate: startUTC + "",
-        endDate: endUTC + ""
-      });
-    } else {
-      let self = this;
-      getProjects()
-        .then(projects => {
-          projects = projects.map(project => {
-            return {
-              value: project.Id,
-              label: project.Name
-            };
-          });
+ 
 
-          projects.unshift({
-            value: "Unavailable",
-            label: "Unavailable"
-          });
+  
 
-          self.addAllocationData = {
-            projects: projects,
-            startDate: startUTC + "",
-            endDate: endUTC + "",
-            disabled: true
-          };
 
-          self.template.querySelector(".add-allocation-modal").show();
-        })
-        .catch(error => {
-          this.dispatchEvent(
-            new ShowToastEvent({
-              message: error.body.message,
-              variant: "error"
-            })
-          );
-        });
-    }
-  }
 
-  handleAddAllocationDataChange(event) {
-    this.addAllocationData[event.target.dataset.field] = event.target.value;
 
-    if (!this.addAllocationData.projectId) {
-      this.addAllocationData.disabled = true;
-    } else {
-      this.addAllocationData.disabled = false;
-    }
-  }
+  
+  
 
-  addAllocationModalSuccess() {
-    if ("Unavailable" === this.addAllocationData.projectId) {
-      this.addAllocationData.projectId = null;
-      this.addAllocationData.status = "Unavailable";
-    }
 
-    this._saveAllocation({
-      projectId: this.addAllocationData.projectId,
-      status: this.addAllocationData.status,
-      startDate: this.addAllocationData.startDate,
-      endDate: this.addAllocationData.endDate
-    })
-      .then(() => {
-        this.addAllocationData = {};
-        this.template.querySelector(".add-allocation-modal").hide();
-      })
-      .catch(error => {
-        this.dispatchEvent(
-          new ShowToastEvent({
-            message: error.body.message,
-            variant: "error"
-          })
-        );
-      });
-  }
+  
 
-  _saveAllocation(allocation) {
-    if (
-      null == allocation.projectId &&
-      null != this.projectId &&
-      !allocation.status
-    ) {
-      allocation.projectId = this.projectId;
-    }
+  
 
-    if (null == allocation.resourceId) {
-      allocation.resourceId = this.resource.Id;
-    }
-
-    return saveAllocation(allocation)
-      .then(() => {
-        // send refresh to top
-        this.dispatchEvent(
-          new CustomEvent("refresh", {
-            bubbles: true,
-            composed: true
-          })
-        );
-      })
-      .catch(error => {
-        this.dispatchEvent(
-          new ShowToastEvent({
-            message: error.body.message,
-            variant: "error"
-          })
-        );
-      });
-  }
-
-  /*** Drag/Drop ***/
-  dragInfo = {};
-  handleDragStart(event) {
-    let container = this.template.querySelector(
-      "." + event.currentTarget.dataset.id + " .lwc-allocation"
-    );
-    this.dragInfo.projectIndex = container.dataset.project;
-    this.dragInfo.allocationIndex = container.dataset.allocation;
-    this.dragInfo.newAllocation = this.projects[
-      container.dataset.project
-    ].allocations[container.dataset.allocation];
-
-    // hide drag image
-    container.style.opacity = 0;
-    setTimeout(function() {
-      container.style.pointerEvents = "none";
-    }, 0);
-  }
-
-  handleLeftDragStart(event) {
-    this.dragInfo.direction = "left";
-    this.handleDragStart(event);
-  }
-
-  handleRightDragStart(event) {
-    this.dragInfo.direction = "right";
-    this.handleDragStart(event);
-  }
-
-  handleDragEnd(event) {
-    event.preventDefault();
-
-    const projectIndex = this.dragInfo.projectIndex;
-    const allocationIndex = this.dragInfo.allocationIndex;
-    const allocation = this.dragInfo.newAllocation;
-
-    this.projects = JSON.parse(JSON.stringify(this.projects));
-    this.projects[projectIndex].allocations[allocationIndex] = allocation;
-
-    let startDate = new Date(allocation.Start_Date__c + "T00:00:00");
-    let endDate = new Date(allocation.End_Date__c + "T00:00:00");
-
-    this._saveAllocation({
-      allocationId: allocation.Id,
-      startDate:
-        startDate.getTime() + startDate.getTimezoneOffset() * 60 * 1000 + "",
-      endDate: endDate.getTime() + endDate.getTimezoneOffset() * 60 * 1000 + ""
-    });
-
-    this.dragInfo = {};
-    this.template.querySelector(
-      "." + allocation.Id + " .lwc-allocation"
-    ).style.pointerEvents = "auto";
-  }
-
-  handleDragEnter(event) {
-    const projectIndex = this.dragInfo.projectIndex;
-    const allocationIndex = this.dragInfo.allocationIndex;
-    const direction = this.dragInfo.direction;
-    const start = new Date(parseInt(event.currentTarget.dataset.start, 10));
-    const end = new Date(parseInt(event.currentTarget.dataset.end, 10));
-    const index = parseInt(event.currentTarget.dataset.index, 10);
-
-    if (isNaN(this.dragInfo.startIndex)) {
-      this.dragInfo.startIndex = index;
-    }
-
-    let allocation = JSON.parse(
-      JSON.stringify(this.projects[projectIndex].allocations[allocationIndex])
-    );
-
-    switch (direction) {
-      case "left":
-        if (index <= allocation.right) {
-          allocation.Start_Date__c = start.toJSON().substr(0, 10);
-          allocation.left = index;
-        } else {
-          allocation = this.dragInfo.newAllocation;
-        }
-        break;
-      case "right":
-        if (index >= allocation.left) {
-          allocation.End_Date__c = end.toJSON().substr(0, 10);
-          allocation.right = index;
-        } else {
-          allocation = this.dragInfo.newAllocation;
-        }
-        break;
-      default:
-        let deltaIndex = index - this.dragInfo.startIndex;
-        let firstSlot = this.times[0];
-        let startDate = new Date(firstSlot.start);
-        let endDate = new Date(firstSlot.end);
-
-        allocation.left = allocation.left + deltaIndex;
-        allocation.right = allocation.right + deltaIndex;
-
-        startDate.setDate(
-          startDate.getDate() + allocation.left * this.dateIncrement
-        );
-        endDate.setDate(
-          endDate.getDate() + allocation.right * this.dateIncrement
-        );
-
-        allocation.Start_Date__c = startDate.toJSON().substr(0, 10);
-        allocation.End_Date__c = endDate.toJSON().substr(0, 10);
-    }
-
-    this.dragInfo.newAllocation = allocation;
-    this.template.querySelector(
-      "." + allocation.Id + " .lwc-allocation"
-    ).style = this.calcStyle(allocation);
-    this.template.querySelector(
-      "." + allocation.Id + " .lwc-allocation-label"
-    ).style = this.calcLabelStyle(allocation);
-  }
-  /*** /Drag/Drop ***/
-
-  openAllocationMenu(event) {
-    let container = this.template.querySelector(
-      "." + event.currentTarget.dataset.id + " .lwc-allocation"
-    );
-    let allocation = this.projects[container.dataset.project].allocations[
-      container.dataset.allocation
-    ];
-
-    if (
-      this.menuData.allocation &&
-      this.menuData.allocation.Id === allocation.Id
-    ) {
-      this.closeAllocationMenu();
-    } else {
-      this.menuData.open = true;
-
-      let projectHeight = this.template
-        .querySelector(".project-container")
-        .getBoundingClientRect().height;
-      let allocationHeight = this.template
-        .querySelector(".lwc-allocation")
-        .getBoundingClientRect().height;
-      let totalSlots = this.times.length;
-      let rightEdge =
-        ((totalSlots - (allocation.right + 1)) / totalSlots) * 100 + "%";
-
-      let topEdge =
-        projectHeight * container.dataset.project + allocationHeight;
-
-      this.menuData.allocation = Object.assign({}, allocation);
-      this.menuData.style =
-        "top: " + topEdge + "px; right: " + rightEdge + "; left: unset";
-    }
-  }
-
-  handleModalEditClick(event) {
-    this.editAllocationData = {
-      resourceName: this.resource.Name,
-      projectName: this.menuData.allocation.projectName,
-      id: this.menuData.allocation.Id,
-      startDate: this.menuData.allocation.Start_Date__c,
-      endDate: this.menuData.allocation.End_Date__c,
-      effort: this.menuData.allocation.Effort__c,
-      status: this.menuData.allocation.Status__c,
-      isFullEdit: this.menuData.allocation.Status__c !== "Unavailable",
-      disabled: false
-    };
-    this.template.querySelector(".edit-allocation-modal").show();
-
-    this.closeAllocationMenu();
-  }
-
-  handleEditAllocationDataChange(event) {
-    this.editAllocationData[event.target.dataset.field] = event.target.value;
-
-    if (
-      !this.editAllocationData.startDate ||
-      !this.editAllocationData.endDate
-    ) {
-      this.editAllocationData.disabled = true;
-    } else {
-      this.editAllocationData.disabled = false;
-    }
-
-    this.editAllocationData.isFullEdit =
-      this.editAllocationData.Status__c !== "Unavailable";
-  }
-
-  editAllocationModalSuccess() {
-    const startDate = new Date(this.editAllocationData.startDate + "T00:00:00");
-    const endDate = new Date(this.editAllocationData.endDate + "T00:00:00");
-
-    this._saveAllocation({
-      allocationId: this.editAllocationData.id,
-      projectId: this.editAllocationData.projectId,
-      startDate:
-        startDate.getTime() + startDate.getTimezoneOffset() * 60 * 1000 + "",
-      endDate:
-        endDate.getTime() + startDate.getTimezoneOffset() * 60 * 1000 + "",
-      effort: this.editAllocationData.effort,
-      status: this.editAllocationData.status
-    })
-      .then(() => {
-        this.editAllocationData = {};
-        this.template.querySelector(".edit-allocation-modal").hide();
-      })
-      .catch(error => {
-        this.dispatchEvent(
-          new ShowToastEvent({
-            message: error.body.message,
-            variant: "error"
-          })
-        );
-      });
-  }
-
-  handleMenuDeleteClick(event) {
-    this.editAllocationData = {
-      id: this.menuData.allocation.Id
-    };
-    this.template.querySelector(".delete-modal").show();
-    this.closeAllocationMenu();
-  }
-
-  handleMenuDeleteSuccess() {
-    deleteAllocation({
-      allocationId: this.editAllocationData.id
-    })
-      .then(() => {
-        this.template.querySelector(".delete-modal").hide();
-        this.dispatchEvent(
-          new CustomEvent("refresh", {
-            bubbles: true,
-            composed: true
-          })
-        );
-      })
-      .catch(error => {
-        this.dispatchEvent(
-          new ShowToastEvent({
-            message: error.body.message,
-            variant: "error"
-          })
-        );
-      });
-  }
 }
